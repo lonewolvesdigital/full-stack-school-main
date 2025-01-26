eff84548b0384ea5c674be7f89ed1aff0a483fff
lonewolvesdigital <lonewolvesdigital@gmail.com>
2025-01-26 14:31:21 +0100

    .svg change

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ export default function DashboardLayout({
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
         >
-          <Image src="/logo.svg" alt="logo" width={32} height={32} />
+          <Image src="/logo.jpeg" alt="logo" width={32} height={32} />
           <span className="hidden lg:block font-bold"> Windward School APP</span>
         </Link>
         <Menu />
